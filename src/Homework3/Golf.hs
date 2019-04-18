81828c6cd87f16b21f8d04115d0a456c86610c8e
Max Yankov <golergka@gmail.com>
2019-04-18 23:41:38 +0300

    Significantly better first assignment

--- src/Homework3/Golf.hs
+++ src/Homework3/Golf.hs
@@ -2,21 +2,8 @@ module Homework3.Golf where
 
 import Debug.Trace
 
-{-
-skipsNRec :: [a] -> Int -> Int -> [a]
-skipsNRec [] _ _ = []
-skipsNRec (x:ys) n 1 = x:(skipsNRec ys n n)
-skipsNRec (x:ys) n i = skipsNRec ys n (i - 1)
-
 skipsN :: [a] -> Int -> [a]
-skipsN xs n = skipsNRec xs n n
--}
-
-isDiv :: Int -> Int -> Bool
-isDiv x y = (mod y x) == 0
-
-skipsN :: [a] -> Int -> [a]
-skipsN xs n = fst (unzip (filter ((isDiv n).snd) (zip xs [1..(length xs)])))
+skipsN xs n = [fst i | i <- (zip xs [1..]), (mod (snd i) n) == 0]
 
 skips :: [a] -> [[a]]
 skips xs = map (skipsN xs) [1..(length xs)]
