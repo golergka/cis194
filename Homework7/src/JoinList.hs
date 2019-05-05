89d8674d1e0982aa1d1f092c3186d99dbe6052a7
Max Yankov <golergka@gmail.com>
2019-05-05 04:39:13 +0300

    indexJ

--- Homework7/src/JoinList.hs
+++ Homework7/src/JoinList.hs
@@ -1,5 +1,7 @@
 module JoinList where
 
+import Sized
+
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
@@ -12,3 +14,18 @@ tag (Append x _ _) = x
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) x y = Append (tag x <> tag y) x y
+
+instance (Sized b, Monoid b) => Sized (JoinList b a) where
+  size jl = (size . tag) jl
+
+indexJ :: (Sized b, Monoid b) =>
+          Int -> JoinList b a -> Maybe a
+indexJ _ Empty                       = Nothing
+indexJ i _ | i < 0                   = Nothing
+indexJ i x | (getSize $ size x) <= i = Nothing
+indexJ 0 (Single _ x)                = Just x
+indexJ i (Append _ a b)
+  | sizeA > i = indexJ i a
+  | otherwise = indexJ (i - sizeA) b
+  where
+    sizeA = getSize $ size a
