6b84f95f6749b13e76e41169b2e3a42401ad7a22
Max Yankov <golergka@gmail.com>
2019-05-05 04:51:16 +0300

    dropJ (and changed types compared to assignment)

--- Homework7/src/JoinList.hs
+++ Homework7/src/JoinList.hs
@@ -15,17 +15,26 @@ tag (Append x _ _) = x
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) x y = Append (tag x <> tag y) x y
 
-instance (Sized b, Monoid b) => Sized (JoinList b a) where
-  size jl = (size . tag) jl
+type SizeList a = JoinList Size a
 
-indexJ :: (Sized b, Monoid b) =>
-          Int -> JoinList b a -> Maybe a
+indexJ :: Size -> SizeList a -> Maybe a
 indexJ _ Empty                       = Nothing
 indexJ i _ | i < 0                   = Nothing
-indexJ i x | (getSize $ size x) <= i = Nothing
+indexJ i x | (tag x) <= i = Nothing
 indexJ 0 (Single _ x)                = Just x
 indexJ i (Append _ a b)
   | sizeA > i = indexJ i a
   | otherwise = indexJ (i - sizeA) b
   where
-    sizeA = getSize $ size a
+    sizeA = tag a
+  
+dropJ :: Size -> SizeList a -> SizeList a
+dropJ 0 x                           = x
+dropJ i x | (tag x) <= i = Empty
+dropJ _ Empty                       = Empty
+dropJ _ (Single _ _)                = Empty
+dropJ i (Append s a b)
+  | sizeA > i = Append (s - i) (dropJ i a) b
+  | otherwise = Append (s - i) a (dropJ (i - sizeA) b)
+  where
+    sizeA = tag a
