6ab2495263e4c39e2544dea771f5a2c7a52acf7a
Max Yankov <golergka@gmail.com>
2019-05-06 01:37:22 +0300

    Homework 8 complete

--- Homework8/src/Main.hs
+++ Homework8/src/Main.hs
@@ -1,3 +1,11 @@
 module Main where
 
-main = error "not implemented"
+import Party
+
+showMaxFun :: String -> String
+showMaxFun = show . maxFun . read
+
+main :: IO ()
+main = putStrLn "Reading from file..." >> 
+       readFile "company.txt" >>= 
+       putStrLn . showMaxFun
