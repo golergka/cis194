d319006d6dfe35966b17625f2d9a2201b955c8c1
Max Yankov <golergka@gmail.com>
2019-05-05 21:35:09 +0300

    Excercise 1: monoid instance

--- Homework8/src/Party.hs
+++ Homework8/src/Party.hs
@@ -1,6 +1,15 @@
 module Party where
 
+import Data.Monoid
+import Data.Semigroup
+
 import Employee
 
 glCons :: Employee -> GuestList -> GuestList
 glCons e (GL list fun) = GL (e:list) (fun + empFun e)
+
+instance Semigroup GuestList where
+  (<>) (GL aList aFun) (GL bList bFun) = GL (aList ++ bList) (aFun + bFun)
+
+instance Monoid GuestList where
+  mempty = GL [] 0
