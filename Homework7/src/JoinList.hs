9c99c2e98507aafa411a814a4d9bf5d110da62b4
Max Yankov <golergka@gmail.com>
2019-05-05 04:10:44 +0300

    Exercise 1

---
+++ Homework7/src/JoinList.hs
@@ -0,0 +1,14 @@
+module JoinList where
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty          = mempty
+tag (Single x _)   = x
+tag (Append x _ _) = x
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) x y = Append (tag x <> tag y) x y
